eae5ca237528ce7db839f4c8418f19d17c129ee3
Facundo Prost <facuprost@gmail.com>
2023-07-03 16:14:19 -0300

    primera entrega 4

--- js/main.js
+++ js/main.js
@@ -5,28 +5,28 @@ const madera = () => {
     let tipo = prompt("¿Qué madera busca entre las siguientes? Eucalipto, pino y saligna");
     switch (tipo) {
       case "eucalipto":
-        console.log(
+        alert(
           "La madera de eucalipto suele ser la mejor en mesas que se utilizan para cocina"
         );
         precioPorMedidaGeneral(tipo);
         seguir = false;
         break;
       case "pino":
-        console.log(
+        alert(
           "Este tipo de madera es la mejor opción para una mesa de comedor o como escritorio"
         );
         precioPorMedidaGeneral(tipo);
         seguir = false;
         break;
       case "saligna":
-        console.log(
+        alert(
           "Esta madera será la más económica y resistente para mesas de exterior"
         );
         precioPorMedidaGeneral(tipo);
         seguir = false;
         break;
       default:
-        console.log("Ingrese un tipo de madera válido");
+        alert("Ingrese un tipo de madera válido");
         seguir = true;
     }
   }
@@ -39,33 +39,33 @@ const precioPorMedidaGeneral = (tipo) => {
     switch (tamanio) {
       case "chica":
         if (tipo === "eucalipto") {
-          console.log("El valor de esta mesa es de $35000");
+          alert("El valor de esta mesa es de $35000");
         } else if (tipo === "pino") {
-          console.log("El valor de esta mesa es de $15000");
+          alert("El valor de esta mesa es de $15000");
         } else if (tipo === "saligna") {
-          console.log("El valor de esta mesa es de $7000");
+          alert("El valor de esta mesa es de $7000");
         }
         break;
       case "mediana":
         if (tipo === "eucalipto") {
-          console.log("El valor de esta mesa es de $85000");
+          alert("El valor de esta mesa es de $85000");
         } else if (tipo === "pino") {
-          console.log("El valor de esta mesa es de $50000");
+          alert("El valor de esta mesa es de $50000");
         } else if (tipo === "saligna") {
-          console.log("El valor de esta mesa es de $30000");
+          alert("El valor de esta mesa es de $30000");
         }
         break;
       case "grande":
         if (tipo === "eucalipto") {
-          console.log("El valor de esta mesa es de $170000");
+          alert("El valor de esta mesa es de $170000");
         } else if (tipo === "pino") {
-          console.log("El valor de esta mesa es de $110000");
+          alert("El valor de esta mesa es de $110000");
         } else if (tipo === "saligna") {
-          console.log("El valor de esta mesa es de $70000");
+          alert("El valor de esta mesa es de $70000");
         }
         break;
       default:
-        console.log("Ingrese un tamaño válido");
+        alert("Ingrese un tamaño válido");
         tamanio = "";
     }
   }
